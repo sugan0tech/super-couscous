27013e73dd08d65e9a40e9363a50328f7f0a6088
sugan0tech <sugankpms@gmail.com>
2023-11-15 17:42:23 +0530

    initial setup

---
+++ grpc-try/src/main/java/org/sugan0tech/grpctry/GrpcTryApplication.java
@@ -0,0 +1,13 @@
+package org.sugan0tech.grpctry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcTryApplication.class, args);
+	}
+
+}
---
+++ grpc-try/src/test/java/org/sugan0tech/grpctry/GrpcTryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sugan0tech.grpctry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
